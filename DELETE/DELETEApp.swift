480399e53af02809bdbfaec0ef9ab39abf73a440
chika <chikha_ali@live.fr>
2021-03-25 10:31:37 +0100

    clonage github

---
+++ DELETE/DELETEApp.swift
@@ -0,0 +1,17 @@
+//
+//  DELETEApp.swift
+//  DELETE
+//
+//  Created by    CHIKA Ali on 08/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct DELETEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
